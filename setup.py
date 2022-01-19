3c2de90ad9bb605148dc6541260c0ad5d692f02b
Adam Weiner <adamcweiner@gmail.com>
2022-01-19 13:31:15 -0500

    convert hyphens to underscores

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
-setup(name='scdna-replication-tools',
+setup(name='scdna_replication_tools',
       version='0.0.1',
       description='Code for analyzing single-cell replication dynamics',
       author='Adam Weiner (Shah Lab)',
-      url='https://github.com/shahcompbio/scdna-replication-tools',
+      url='https://github.com/shahcompbio/scdna_replication_tools',
       packages=find_packages(),
       entry_points={
         'console_scripts': [
-            'infer_SPF = scdna-replication-tools.infer_SPF:main',
-            'infer_scRT = scdna-replication-tools.infer_scRT:main'
+            'infer_SPF = scdna_replication_tools.infer_SPF:main',
+            'infer_scRT = scdna_replication_tools.infer_scRT:main'
         ]
       }
     )
