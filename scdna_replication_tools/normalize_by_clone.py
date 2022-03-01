1f1401e1d2293c9796717e07dd61d3c8ac514aa6
Adam Weiner <adamcweiner@gmail.com>
2022-03-01 16:41:28 -0500

    fix main() and get_args() for normalize_by_clone.py

--- scdna_replication_tools/normalize_by_clone.py
+++ scdna_replication_tools/normalize_by_clone.py
@@ -12,10 +12,8 @@ def get_args():
     p = ArgumentParser()
 
     p.add_argument('s_phase_cells', help='copynumber tsv file with state, copy, clone_id, etc for s-phase cells')
-    p.add_argument('clone_copy', help='output clone copynumber copy tsv file')
-    p.add_argument('clone_reads', help='output clone copynumber reads tsv file')
-    p.add_argument('clone_states', help='output clone copynumber reads tsv file')
-    p.add_argument('clone_rpm_gc_norm', help='output clone copynumber gc corrected reads per million tsv file')
+    p.add_argument('clone_profiles', help='consensus clone profiles in matrix format')
+    p.add_argument('input_col', help='column within s_phase_cells to use as input')
     p.add_argument('output', help='same as s_phase_cells input but with new column for copy_norm')
 
     return p.parse_args()
@@ -50,7 +48,6 @@ def cell_clone_norm(clone_profiles, cell_cn, clone_id, input_col, temp_col):
     return out
 
 
-
 def normalize_by_clone(cn_s, clone_profiles, input_col='rpm_gc_norm', clone_col='clone_id',
                        temp_col='temp_rt', output_col='rt_value', seg_col='changepoint_segments'):
     # drop loci with nans
@@ -82,39 +79,13 @@ def normalize_by_clone(cn_s, clone_profiles, input_col='rpm_gc_norm', clone_col=
     return cn_s_output
 
 
-
-
 def main():
     argv = get_args()
-    df = pd.read_csv(argv.s_phase_cells, sep='\t', dtype={'chr': str})
+    cn_s = pd.read_csv(argv.s_phase_cells, sep='\t', dtype={'chr': str})
+    clone_profiles = pd.read_csv(argv.clone_profiles, sep='\t')
 
-    # drop the Y chromosome
-    df = df[df['chr'] != 'Y']
+    output_df = normalize_by_clone(cn_s, clone_profiles, argv.input_col)
 
-    clone_idx = ['chr', 'start', 'end']
-    clone_copy = pd.read_csv(argv.clone_copy, sep='\t', index_col=clone_idx)
-    clone_reads = pd.read_csv(argv.clone_reads, sep='\t', index_col=clone_idx)
-    clone_states = pd.read_csv(argv.clone_states, sep='\t', index_col=clone_idx)
-    clone_rpm_gc_norm = pd.read_csv(argv.clone_rpm_gc_norm, sep='\t', index_col=clone_idx)
-
-    df = add_cell_ploidies(df)
-
-    # loop through each cell and divide copy by its corresponding clone
-    output_list = []
-    for cell_id, cell_cn in df.groupby('cell_id'):
-        # set index to match clone dfs
-        temp_cell_cn = cell_cn.set_index(clone_idx).copy()
-        # extract the assigned clone_id
-        temp_clone_id = temp_cell_cn.clone_id.unique()[0]
-        # add normalized copy column (with new indices)
-        temp_out_cn = cell_clone_norm(clone_copy, clone_reads, clone_states, clone_rpm_gc_norm, temp_cell_cn, temp_clone_id)
-        # remove cell specific CNAs by nominating changepoints
-        temp_out_cn = remove_cell_specific_CNAs(temp_out_cn)
-
-        output_list.append(temp_out_cn)
-
-    # convert list to df and save as tsv
-    output_df = pd.concat(output_list)
     output_df.to_csv(argv.output, sep='\t', index=False)
 
 
