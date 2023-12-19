3d5ca5b2c543d723dc22c7f648549648fb2ac5b7
Adam Weiner <adamcweiner@gmail.com>
2023-12-19 16:26:00 -0500

    updates required for publishing on PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup, find_packages
 
+with open('requirements_setup.txt') as f:
+    requirements = f.read().splitlines()
+
+with open('dependency_links.txt') as f:
+    dependency_links = f.read().splitlines()
+  
+# remove elements from requirements if the first character is a '#'
+requirements = [x for x in requirements if x[0] != '#']
+dependency_links = [x for x in dependency_links if x[0] != '#']
+
 setup(name='scdna_replication_tools',
       version='0.0.1',
       description='Code for analyzing single-cell replication dynamics',
       author='Adam Weiner (Shah Lab)',
       url='https://github.com/shahcompbio/scdna_replication_tools',
       packages=find_packages(),
+      install_requires=requirements,
+      dependency_links=dependency_links,
       entry_points={
         'console_scripts': [
             'infer_SPF = scdna_replication_tools.infer_SPF:main',
